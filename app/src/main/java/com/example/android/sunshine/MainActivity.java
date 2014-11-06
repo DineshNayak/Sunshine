46acb67d2b3ceb60f7e5768a5c0e2bd351ccf59d
Dinesh <dineshnayakre@gmail.com>
2014-11-06 19:07:01 +0530

    Lesson 2 half done till refresh button

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -1,23 +1,9 @@
 package com.example.android.sunshine;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 
 public class MainActivity extends ActionBarActivity {
 
@@ -25,20 +11,21 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.drawable.ic_launcher);
     }
 
-    @Override
+   //@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+       getMenuInflater().inflate(R.menu.menu_main, menu);
+       return true;
     }
 
     @Override
@@ -53,50 +40,8 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
 
+
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        private ArrayAdapter<String> mForecastAdapter;
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-
-        String[] forecastArray = {
-                "Today - Sunny - 88/63",
-                "Tomorrow - Foggy - 70/40",
-                "Weds - Cloudy - 72/62",
-                "Thurs - Asteroids - 75/65",
-                "Fri - Heavy Rain - 65/56",
-                "Sat - Help Trapped In Weather Station - 60/51",
-                "Sun - Sunny - 80/68",
-                "Mon - Sunny - 78/65",
-                "Tue - Cloudy - 68/60",
-                "Weds - Cloudy - 72/62",
-                "Thurs - Rain - 65/57"
-        };
-
-            List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
-
-            mForecastAdapter =
-                new ArrayAdapter<String>(
-                        getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textview,weekForecast
-                );
-
-
-            ListView listview =(ListView)rootView.findViewById(R.id.listview_forecast);
-            listview.setAdapter(mForecastAdapter);
-            return rootView;
-        }
-    }
 }
